759ffbaf20d7f7c1446a7010f14efcc710a17190
peterletkemansstechus <94179929+peterletkemansstechus@users.noreply.github.com>
2022-02-15 15:36:47 -0500

    file fixes

--- messenger/sample.js
+++ messenger/sample.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Message = require('./message');
 
 let message = new Message().MessageBuilder()
@@ -11,18 +10,4 @@ let message = new Message().MessageBuilder()
             .withFromUser('fromUser7')
             .build();
 
-=======
-const Message = require('./message');
-
-let message = new Message().MessageBuilder()
-            .withStatus('status1')
-            .withSubject('subject2')
-            .withBody('body3')
-            .withMsgTime('msgTime4')
-            .withReaction('reaction5')
-            .withToUser('toUser6')
-            .withFromUser('fromUser7')
-            .build();
-
->>>>>>> 79092633434a3491f677bd0a21fe7e50e3446a66
 console.log(JSON.stringify(message));
